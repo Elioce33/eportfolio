7addd79a92df35267021c18f2e69d074a3bf709d
eliott <eliott.collin@bettercalldave.io>
2023-07-17 14:06:38 +0200

    init nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import '../public/styles/main.css'
+
+export default function Page() {
+  return <div className='main'>
+          <h1>Eliott Collin</h1>
+          <span>I'm working on it... <i>I swear</i></span>
+        </div>
+}
