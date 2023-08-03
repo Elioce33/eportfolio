7b48ca13fa056a3f958093c964f96689c6700e3e
eliott <eliott.collin@bettercalldave.io>
2023-08-03 14:04:53 +0200

    feat: add main content style

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import '../public/styles/main.css'
 
 export default function Page() {
-  return <div className='main'>
-          <h1>Eliott Collin</h1>
-          <span>I'm working on it... <i>I swear</i></span>
-        </div>
+  return <>
+    <h1>Eliott Collin</h1>
+    <div className='main'>
+      <article>
+        <h2>Project</h2>
+      </article>
+      <article>
+        <h2>School career</h2>
+      </article>
+      <article>
+        <h2>Work experience</h2>
+      </article>
+    </div>
+  </>
 }
