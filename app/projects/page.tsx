a59a5aafd228f2504ca02c098d4629ea48a7a1fd
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2023-08-21 23:08:06 +0200

    wip: change bubble positions

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,8 +2,9 @@ import {Bubble, BubbleProject} from "@/app/components/bubble";
 import {projects} from "./projects.json";
 import {Project} from "@/app/types";
 
-const projectsItems = projects.map((project: Project, index:  number) => {
-    return <BubbleProject key={index} project={project}/>
+const projectsItems = projects.map((project: Project, index: number) => {
+  const position = Math.floor(Math.random() * 4 + 1);
+  return <BubbleProject key={index} project={project} display_option={{size: 2, position}}/>
 });
 
 export default function Page() {
