b755e0ab358f70b2faa45f24435e34b70b3fb594
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2023-08-26 23:12:27 +0200

    refactor: remove useless component BubbleProject

--- components/bubble.tsx
+++ components/bubble.tsx
@@ -13,31 +13,3 @@ export function Bubble({className = "", title, children}: any) {
         </div>
     </div>;
 }
-
-export function BubbleProject({project, display_option}: any) {
-    const {name, slug, description, languages, technologies, links} = project;
-    const languagesItems = languages.map((language: string, index: number) => <li key={index}>{language}</li>);
-    const technologiesItems = technologies.map((technology: string, index: number) => <li key={index}>{technology}</li>);
-    const linksItems = links.map(({url, name}: {url: string, name: string}, index: number) => <li key={index}><a href={url}>{name}</a></li>);
-
-    const title = <Link href={"projects/" + slug}>{name}</Link>;
-
-    return <Bubble className="project_bubble" title={title} display_option={display_option}>
-        <p>{description}</p>
-
-        <div className="project_tools">
-            {languages.length > 0 && <>
-                <h3>Language</h3>
-                <ul>{languagesItems}</ul>
-            </>}
-            {technologies.length > 0 && <>
-                <h3>Technologies</h3>
-                <ul>{technologiesItems}</ul>
-            </>}
-        </div>
-        {links.length > 0 && <>
-            <h3>Links</h3>
-            <ul>{linksItems}</ul>
-        </>}
-    </Bubble>
-}
