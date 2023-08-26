092d970b9ab3c185c38e55b502d6c719aa3897be
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2023-08-26 22:55:05 +0200

    feat: change schools and certificate displaying

---
+++ components/article.school.tsx
@@ -0,0 +1,24 @@
+import {Article} from "@/components/article";
+import Link from "next/link";
+
+
+export function ArticleSchool({className = "", children, school}: any) {
+  const {slug, name, address, url, logo} = school;
+
+  return <>
+    <Article className={`school-${slug} ${className}`}>
+      <header className="school-header row">
+        <div className="col">
+          <h2><Link href={url} target="_blank">{name}</Link></h2>
+          <span>{address}</span>
+        </div>
+        <div className="school-logo col text-end">
+          <img src={`/images/schools/${slug}.png`} className="img-fluid" alt={`Logo de ${name}`}/>
+        </div>
+      </header>
+      <div className="certificates">
+        {children}
+      </div>
+    </Article>
+  </>
+}
