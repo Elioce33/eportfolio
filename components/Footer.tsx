5a7c52c31f1cb204471eb248adad1360db2604f6
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2023-09-07 16:28:29 +0200

    feat: add contact links in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,11 @@ import {LinkList} from "@components/LinkList";
 
 export function Footer() {
   const links = [
-    {label: "Github", slug: "https://github.com/elioce33", blank: true},
-    {label: "Gitlab", slug: "https://gitlab.com/eliott.collin", blank: true},
-    {label: "Linkedin", slug: "https://www.linkedin.com/in/eliottcollin/", blank: true},
+    {label: "Github", slug: "https://github.com/elioce33", blank: true, icon: "bi bi-github"},
+    {label: "Gitlab", slug: "https://gitlab.com/eliott.collin", blank: true, icon: "bi bi-gitlab"},
+    {label: "Linkedin", slug: "https://www.linkedin.com/in/eliottcollin/", blank: true, icon: "bi bi-linkedin"},
+    {label: "Mail", slug: "mailto:eliott.collin0307@gmail.com", blank: true, icon: "bi bi-envelope"},
+    {label: "Téléphone", slug: "tel:+336 51 29 28 50", blank: true, icon: "bi bi-telephone"},
   ];
 
   return <footer>
@@ -17,7 +19,7 @@ export function Footer() {
           <SiteMap/>
         </div>
         <div className={"link col"}>
-          <h3 className={"h5"}>Utils links</h3>
+          <h3 className={"h5"}>Contact</h3>
           <LinkList links={links}/>
         </div>
       </div>
