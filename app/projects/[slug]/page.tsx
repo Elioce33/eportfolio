102bf7e543e42ea6805cc718598f64bcc1f74e69
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2023-09-08 16:52:33 +0200

    feat: add bubble nav in project/slug

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,11 @@
+import styles from "@/app/projects/page.module.css";
+
 import {projects} from "../projects.json";
+
 import Link from "next/link";
 import {LinkList} from "@components/LinkList";
+import {Bubble} from "@components/Bubble";
+
 
 export default function Page({ params }: { params: { slug: string } }) {
   const project: any = projects.find((project) => project.slug === params.slug);
@@ -15,8 +20,11 @@ export default function Page({ params }: { params: { slug: string } }) {
 
   return <>
     <main className="project container-fuild p-3">
-      <nav className={"projectList"}>
-        <LinkList title={"Projects :"} baseLink={"/projects"} links={projectLinkList}/>
+      <nav className={styles.projectList}>
+        <Bubble className={styles.bubble}>
+          <h2>Projects&nbsp;:</h2>
+          <LinkList baseLink={"/projects"} links={projectLinkList}/>
+        </Bubble>
       </nav>
 
       <h2>{name}</h2>
